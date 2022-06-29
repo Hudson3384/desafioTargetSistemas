29f9df477c8baee43cb3b38ffce4632cc45c9054
hudson3384 <hudson.arruda@outlook.com>
2022-06-29 16:09:33 -0300

    Conclusão do desafio 3

--- script.js
+++ script.js
@@ -32,7 +32,173 @@ function desafioFibonnaci() {
 }
 
 
-async function start(){
-    const url = 'https://drive.google.com/file/d/1qXvCPjEL4jerElN-hnScoKkEVmSQ8A2L'
+
+(function (){
+    var resDesafioTerceiro = document.body.getElementsByClassName('AnswerThirdChallenge')
+    const dados = JSON.parse(`[
+        {
+            "dia": 1,
+            "valor": 22174.1664
+        },
+        {
+            "dia": 2,
+            "valor": 24537.6698
+        },
+        {
+            "dia": 3,
+            "valor": 26139.6134
+        },
+        {
+            "dia": 4,
+            "valor": 0.0
+        },
+        {
+            "dia": 5,
+            "valor": 0.0
+        },
+        {
+            "dia": 6,
+            "valor": 26742.6612
+        },
+        {
+            "dia": 7,
+            "valor": 0.0
+        },
+        {
+            "dia": 8,
+            "valor": 42889.2258
+        },
+        {
+            "dia": 9,
+            "valor": 46251.174
+        },
+        {
+            "dia": 10,
+            "valor": 11191.4722
+        },
+        {
+            "dia": 11,
+            "valor": 0.0
+        },
+        {
+            "dia": 12,
+            "valor": 0.0
+        },
+        {
+            "dia": 13,
+            "valor": 3847.4823
+        },
+        {
+            "dia": 14,
+            "valor": 373.7838
+        },
+        {
+            "dia": 15,
+            "valor": 2659.7563
+        },
+        {
+            "dia": 16,
+            "valor": 48924.2448
+        },
+        {
+            "dia": 17,
+            "valor": 18419.2614
+        },
+        {
+            "dia": 18,
+            "valor": 0.0
+        },
+        {
+            "dia": 19,
+            "valor": 0.0
+        },
+        {
+            "dia": 20,
+            "valor": 35240.1826
+        },
+        {
+            "dia": 21,
+            "valor": 43829.1667
+        },
+        {
+            "dia": 22,
+            "valor": 18235.6852
+        },
+        {
+            "dia": 23,
+            "valor": 4355.0662
+        },
+        {
+            "dia": 24,
+            "valor": 13327.1025
+        },
+        {
+            "dia": 25,
+            "valor": 0.0
+        },
+        {
+            "dia": 26,
+            "valor": 0.0
+        },
+        {
+            "dia": 27,
+            "valor": 25681.8318
+        },
+        {
+            "dia": 28,
+            "valor": 1718.1221
+        },
+        {
+            "dia": 29,
+            "valor": 13220.495
+        },
+        {
+            "dia": 30,
+            "valor": 8414.61
+        }
+    ]`)
+let dadosFiltrados = []
+ dados.forEach(element => {
+    if(element.valor !== 0){
+        dadosFiltrados.push(element)
+    }
+})
+//Valor máxiimo e Mínimo
+let min = {valor: Infinity}
+let max = {valor: -Infinity}
+for ( let i of dadosFiltrados) {
+    min = i.valor < min.valor ? i : min
+    max = i.valor > max.valor ? i : max 
 }
 
+//soma dos valores: 
+let sum = 0; 
+dadosFiltrados.forEach(i => sum+=i.valor )
+
+//média dos rendimentos 
+const media = sum/dadosFiltrados.length //dias que não tiveram rendimento zero
+
+let diasAcimaMedia = 0;
+dadosFiltrados.forEach(i => {
+    if (i.valor > media) {
+         diasAcimaMedia++
+    }
+   
+})
+resDesafioTerceiro.innerHTML = `O menor valor foi de ${min.valor}, o maior de ${max.valor} e a quantidade
+de dias que ultrapassaram a média foi de ${diasAcimaMedia} dias`
+
+})()
+
+// (function(){
+//     const fatGeral = {
+//         SP: 67836.43,
+//         RJ: 36678.66,
+//         MG: 29229.88,
+//         ES: 27165.48,
+//         Outros: 19849.53}
+//     const fatTotal = 0;
+//     //fatGeral.forEach(i => fatTotal+=i.)
+//     console.log(fatTotal)
+// })()
+    
